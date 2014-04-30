2f9cf2714d0e11718ee3a864e46f9825972ddc35
Chris Cacciatore <chris.cacciatore@gmail.com>
2014-04-30 08:40:16 -0700

    Whitespace formatting.

--- lib/logan/calendar_event.rb
+++ lib/logan/calendar_event.rb
@@ -4,9 +4,9 @@ module Logan
   class CalendarEvent
     include HashConstructed
     
-    attr_accessor :id
-    attr_accessor :summary
-    attr_accessor :starts_at
+		attr_accessor :id
+		attr_accessor :summary
+		attr_accessor :starts_at
 		attr_accessor :creator
   end  
 end
